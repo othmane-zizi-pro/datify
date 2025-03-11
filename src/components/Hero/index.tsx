0ff7c0ebde339cb5728b6a1ff993d84e92f2fc5f
Othmane Zizi <othmane@uni.minerva.edu>
2025-03-11 15:53:50 -0400

    ok

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,13 +12,13 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Free and Open-Source Next.js Template for Startup & SaaS
+                Tech Consulting for Startups & SMBs
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Startup is free Next.js template for startups and SaaS
-                  business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
-                  business website, built-with Next 13.x and Tailwind CSS.
+                At Datify, we empower tech startups and small-to-medium businesses with cutting-edge software solutions. 
+                Our consulting expertise enhances both mission-critical operations and support functions, ensuring your business runs 
+                smarter and scales faster. Our customized strategies and implementations 
+                deliver measurable results. Partner with Datify to optimize your technology and achieve your goals.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
