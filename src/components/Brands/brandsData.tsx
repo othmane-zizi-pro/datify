943b1da5c61dd800fe3fc6de10676d9162ffabb7
Othmane Zizi <othmane@uni.minerva.edu>
2025-03-15 01:39:36 -0400

    okok

--- src/components/Brands/brandsData.tsx
+++ src/components/Brands/brandsData.tsx
@@ -3,22 +3,22 @@ import { Brand } from "@/types/brand";
 const brandsData: Brand[] = [
   {
     id: 6,
-    name: "Formbold",
-    href: "https://formbold.com",
+    name: "Integral Treasury",
+    href: "https://integral.xyz/",
     image: "/images/brands/integral.svg",
     imageLight: "/images/brands/integral-light.svg",
   },
   {
     id: 2,
-    name: "Tailgrids",
-    href: "https://tailgrids.com",
+    name: "Origin Financial",
+    href: "https://www.useorigin.com/",
     image: "/images/brands/origin.svg",
     imageLight: "/images/brands/origin-light.svg",
   },
   {
     id: 1,
-    name: "UIdeck",
-    href: "https://uideck.com",
+    name: "AMS Africa",
+    href: "https://www.amsafrica-realestate.com/",
     image: "/images/brands/ams.svg",
     imageLight: "/images/brands/ams-light.svg",
   },
